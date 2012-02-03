a8ce172e95ec77aaaef5d21a4994962a30b85cb6
Raphaël Proust <raphlalou@gmail.com>
2012-02-03 14:37:43 +0000

    [arp] some documentation

--- examples/arp/arp.ml
+++ examples/arp/arp.ml
@@ -2,6 +2,8 @@
 open Froc_sa
 open Nettypes
 
+(*TODO: Lwt-ise*)
+
 
 
 module ARP : sig
@@ -9,15 +11,36 @@ module ARP : sig
   module M : (Map.S with type key = ipv4_addr)
   module S : (Set.S with type elt = ipv4_addr)
 
+  (*If not initialised, other functions of this module might raise [Failure]*)
   val init: output:(arp -> unit) -> get_mac:(unit -> ethernet_mac) -> unit
+
+
+  (* This is exposed for now, but should probably be hidden. As it is read-only,
+   * the icentive to hide it is small.
+   * Prefer [query] for querying a specific value.
+   *)
   val cache: ethernet_mac M.t Froc_sa.t
+
+  (* Use this when receiving an [arp] *)
   val input: arp -> unit
+
+  (* The set of ip adresses local to this system. *)
   val bound_ips: S.t Froc_sa.t
+
+  (* Use these to change the set of local ip addresses. *)
   val set_bound_ips: S.t -> unit
   val add_bound_ip: ipv4_addr -> unit
   val rem_bound_ip: ipv4_addr -> unit
+
+  (* sends a arp request for the given ip address. This should be hidden as it
+   * is automatically called when using [query].
+   *)
   val send_request: ipv4_addr -> unit
 
+  (* queries a given ip address. The returned changeable might initially be
+   * [None], in which case a request is sent and the changeable will change when
+   * the replies come.
+   *)
   val query: ipv4_addr -> ethernet_mac option Froc_sa.t
 
 end = struct
