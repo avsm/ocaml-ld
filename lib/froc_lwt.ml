dadc7b6539b33eb8dcd1878bbdb2d2ec37637fd3
Raphaël Proust <raphlalou@gmail.com>
2012-02-07 11:22:54 +0000

    [lib] DOs and DONTs warnings

--- lib/froc_lwt.ml
+++ lib/froc_lwt.ml
@@ -1,4 +1,20 @@
 
+(*DOs and DONTs (although, only DONTs right now
+ *
+ * DONT expose writables (because it exposes the next DONT to the user)
+ * DONT change and yield (or otherwise switch thread)
+ *      This can lead to partial changes being propagated. If you need to change
+ *      several values before propagation, treat all the changes as a critical
+ *      section, and make sure you propagate before yielding.
+ * DONT bypass Froc_sa.bind by creating a new changeable, manually write and
+ *      propagate.
+ * DONT implement events over changeables. This restriction ensures that the
+ *      whole system (in particullar, the data-plan) will be reactive (although
+ *      theoretically appealing, it is better, for performance reasons, to
+ *      restrict the reactive part to the configuration-plan).
+ *)
+
+
 let stream_of_changeable c =
   let (s, push) = Lwt_stream.create () in
   let really_push x = push (Some x) in
